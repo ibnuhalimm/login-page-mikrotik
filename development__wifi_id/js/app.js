02b0c5cbbc6fcbf17b07b49731e3c65310a9a6b1
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2020-10-18 18:21:20 +0700

    add development like wifi_id

---
+++ development__wifi_id/js/app.js
@@ -0,0 +1,10 @@
+$('.tab a').click(function(e) {
+   e.preventDefault();
+   let tab_id = $(this).attr('href');
+
+   $(this).addClass('bg-red-600')
+       .parent().siblings().children()
+       .removeClass('bg-red-600').addClass('bg-gray-900');
+
+   $('.tab-panel '+tab_id).show().siblings().hide();
+});
