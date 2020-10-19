315d918beff5d45a97e0f4eae4f44cbdc57de119
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2020-10-19 14:04:48 +0700

    enhanched tailwind scaffolding

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,20 @@ module.exports = {
   purge: [
     './src/**/*.html',
     './src/**/*.vue',
-    './src/**/*.jsx',
-    './development/*.html'
+    './src/**/*.jsx'
   ],
   theme: {
     extend: {
       colors: {
-        'color-one': '#90FCF9',
-        'color-second': '#63B4D1',
-        'color-third': '#7699D4',
-        'color-fourth': '#9448BC',
-        'color-fifth': '#480355',
-        'red-error': '#FF2929',
-        'yellow-error': '#FFF629',
-        'gray-unstable': '#444444',
-        'whatsapp-green': '#25D366'
+        // 'color-one': '#90FCF9',
+        // 'color-second': '#63B4D1',
+        // 'color-third': '#7699D4',
+        // 'color-fourth': '#9448BC',
+        // 'color-fifth': '#480355',
+        // 'red-error': '#FF2929',
+        // 'yellow-error': '#FFF629',
+        // 'gray-unstable': '#444444',
+        // 'whatsapp-green': '#25D366'
       }
     },
   },
